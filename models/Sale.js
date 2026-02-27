1b17d5650c01bad7aa6dbac3c2197329b597c67c
izeeshan007 <104321250+izeeshan007@users.noreply.github.com>
2026-02-27 09:58:22 +0530

    Add files via upload

--- models/Sale.js
+++ models/Sale.js
@@ -1,25 +1,22 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const SaleSchema=new mongoose.Schema({
-
- category:String,
- productName:String,
- designerName:String,
-
- size_ml:Number,
- units:Number,
-
- customerName:String,
- referenceSource:String,
-
- manufacturingCost:Number,
- soldPrice:Number,
-
- profit:Number,
- profitPercent:Number,
-
- saleDate:{type:Date,default:Date.now}
-
+const SaleSchema = new mongoose.Schema({
+  category: String,
+  productName: String,
+  designerName: String,
+  size_ml: Number,
+  units: Number,
+  customerName: String,
+  referenceSource: String,
+  manufacturingCost: Number,
+  soldPrice: Number,
+  profit: Number,
+  profitPercent: Number,
+  // --- New Fields ---
+  isDelivered: { type: Boolean, default: false },
+  isPaid: { type: Boolean, default: false },
+  // ------------------
+  saleDate: { type: Date, default: Date.now }
 });
 
-module.exports=mongoose.model("Sale",SaleSchema);
+module.exports = mongoose.model("Sale", SaleSchema);
